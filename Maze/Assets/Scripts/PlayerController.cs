2a506a6d2894a3ae4ea0b040b30a5d6bd2005690
Jimmy Chang <jimmyfchang94@gmail.com>
2018-08-22 14:33:00 -0700

    the start of a maze game

---
+++ Maze/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    private Vector3 movement;
+
+    public float speed;
+
+    public Text winText;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        winText.text = "";
+	}
+	
+	void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+	}
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject.CompareTag("WinTrigger"))
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
